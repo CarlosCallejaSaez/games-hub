5672fcacb49493e19e6043af73f64aa37895acc0
CarlosCallejaSaez <carloscallejasez@gmail.com>
2023-06-24 18:15:17 +0200

    protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,41 @@
 import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./components/Home";
-import { Routes, Route,Navigate } from "react-router-dom";
 import TicTacToe from "./components/TicTacToe";
 import HangmanGame from "./components/HangmanGame";
 import SudokuGame from "./components/SudokuGame";
 import LoginForm from "./components/LoginForm";
 
 const App = () => {
-  const isLoggedIn = localStorage.getItem("user");
+  const isLoggedIn = !!localStorage.getItem("user");
+
+  const ProtectedRoute = ({ path, element }) => {
+    return isLoggedIn ? (
+      element
+    ) : (
+      <Navigate to="/" replace state={{ from: path }} />
+    );
+  };
+
   return (
-    <>
-    
-      <Routes>
-        <Route path="/" element={<LoginForm/>} />
-        <Route path="/home" element={isLoggedIn ? <Home /> : <Navigate to="/" />} />
-        <Route path="/tictactoe" element={<TicTacToe/>} />
-        <Route path="/hangman" element={<HangmanGame/>} />
-        <Route path="/sudoku" element={<SudokuGame/>} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<LoginForm />} />
+      <Route path="/home" element={<ProtectedRoute element={<Home />} />} />
+      <Route
+        path="/tictactoe"
+        element={<ProtectedRoute element={<TicTacToe />} />}
+      />
+      <Route
+        path="/hangman"
+        element={<ProtectedRoute element={<HangmanGame />} />}
+      />
+      <Route
+        path="/sudoku"
+        element={<ProtectedRoute element={<SudokuGame />} />}
+      />
+
+      
+    </Routes>
   );
 };
 
